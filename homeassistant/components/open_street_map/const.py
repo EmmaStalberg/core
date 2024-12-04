413611e11e6b52ca134acb2fb14819953331b1ad
EmmaStalberg <74003232+EmmaStalberg@users.noreply.github.com>
2024-12-04 13:42:55 +0000

    Added some constants that are needeed in osm integration

--- homeassistant/components/open_street_map/const.py
+++ homeassistant/components/open_street_map/const.py
@@ -1,3 +1,15 @@
 """Constants for the OpenStreetMap integration."""
 
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from homeassistant.util.hass_dict import HassKey
+
+if TYPE_CHECKING:
+    from homeassistant.helpers.entity_component import EntityComponent
+
+    from . import OpenStreetMapEntity
+
 DOMAIN = "open_street_map"
+DOMAIN_DATA: HassKey[EntityComponent[OpenStreetMapEntity]] = HassKey(DOMAIN)
