c4b17d182a609a2b8f95bb01da71e52dfc13d1df
Izabell Arvidsson <izabell@student.chalmer.se>
2024-12-06 07:50:20 +0000

    Created shareLocation and added it to the service.yaml

---
+++ homeassistant/components/open_street_map/shareLocation.py
@@ -0,0 +1,16 @@
+"""Functionality to return a url link."""
+
+
+# Generate an OpenStreetMap URL centered on given coordinates
+def generate_map_url(lat: float, lon: float) -> str:
+    """Generate a URL for OpenStreetMap centered on the given coordinates.
+
+    Args:
+        lat (float): Latitude of the location.
+        lon (float): Longitude of the location.s
+
+    Returns:
+        str: The OpenStreetMap URL.
+
+    """
+    return f"https://www.openstreetmap.org/?mlat={lat}&mlon={lon}#map=16/{lat}/{lon}"
