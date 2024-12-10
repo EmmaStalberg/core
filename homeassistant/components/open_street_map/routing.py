ed4dd518e40127a2220f4db12f4cfed6407d5e2e
david-ferm00 <david@ferm.co.uk>
2024-12-10 12:44:25 +0000

    added routing functionality to backend as well as finding restaurants nearby

---
+++ homeassistant/components/open_street_map/routing.py
@@ -0,0 +1,44 @@
+"""Module to search for addresses or coordinates using the OpenStreetMap API."""
+
+import requests
+
+PROJECT_OSRM_URL = "http://router.project-osrm.org/route/v1/driving/%d,%d;%d,%d?"
+OVERPASS_URL = 'https://overpass-api.de/api/interpreter?data=[out:json];node["amenity"="restaurant"](around:500,%d,%d);out;'
+
+
+def fetch_route(start_coordinates : dict[str, float], end_coordinates : dict[str, float]):
+    '''Find route between two coordinates.'''
+
+    params = {"steps": "true", "geometries": "geojson", "overview": "full"}
+
+    try:
+        response = requests.get(
+                (PROJECT_OSRM_URL % str(start_coordinates["lon"]), str(start_coordinates["lat"]), str(end_coordinates["lon"]), str(end_coordinates["lat"])),
+                params=params,
+                timeout=5
+            )
+        result = response.json()
+    except requests.exceptions.Timeout:
+        return {"error": "Request timed out"}
+    except requests.exceptions.RequestException as error:
+        return {"error": f"Request failed: {error}"}
+
+    if result.routes and result.routes.length>0:
+        return result.routes[0].geometry
+
+    return {"error": "No route found"}
+
+def find_restaurants_near(coordinates : dict[str, float], count : int):
+    """Find restaurants near route where it is clicked."""
+
+    try:
+        response = requests.get(
+                (OVERPASS_URL % coordinates["lat"], coordinates["lon"]),
+                timeout=5
+            )
+        result = response.json()
+        return result.elements.slice(0, count)
+    except requests.exceptions.Timeout:
+        return {"error": "Request timed out"}
+    except requests.exceptions.RequestException as error:
+        return {"error": f"Request failed: {error}"}
